29e9f042591f21ebebb70f360dd4e314b00c0705
Maria Luiza Neiva <marialqneiva@gmail.com>
2025-05-05 15:05:53 -0300

    Delete exercício1.py

--- exercício1.py
+++
@@ -1,6 +0,0 @@
-x=int(input('Digite o primeiro valor: '))
-y=int(input('Digite o segundo valor: '))
-if x>y:
-    print(f'A ordem crescente dos valores é: {y}, {x}.')
-else:
-    print(f'A ordem crescente dos valores é: {x}, {y}.')
